25a15ffaf60492dfa1accd7a712cb6272a899740
Imransk7768 <imransk7768@gmail.com>
2022-04-22 15:58:59 +0530

    [Imran] Added: Welcome To AddressBook

--- AddressBookProgram/Contacts.cs
+++ AddressBookProgram/Contacts.cs
@@ -12,9 +12,9 @@ namespace AddressBookProgram
         public string LastName { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+        public int Mobile { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public string ZipCode { get; set; }
+        public int ZipCode { get; set; }
     }
 }
